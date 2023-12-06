9485d4f4bbb1550de620130c16d64d3601abcb01
DKeshavarz <dankeshavarz1075@gmail.com>
2023-12-05 22:52:10 -0900

    solving 5...

--- HW2_3.cpp
+++ HW2_3.cpp
@@ -4,6 +4,41 @@
 
 using namespace std;
 
+
+struct node
+{
+    int data {};
+    node* left {nullptr};
+    node* right {nullptr};
+};
+
+int findKey (int* arr, int start, int end ,int key)
+{
+    For(i,start,end)
+        if(arr[i] == key)
+            return i;
+
+    cerr << "fucckkk key not found\n";
+    return -1;
+}
+
+node* fucn (int* arrInorder,int* arrPostOrder,int start , int end)
+{
+    cout << "root :" << arrPostOrder[end-1] << '\n';
+
+    int rootIndex = findKey(arrInorder,start,end,arrPostOrder[end-1]);
+
+    cout << "left: ";
+    For(i,start,rootIndex)
+        cout << arrInorder[i] << " ";
+
+    cout << "\nright: ";
+    For(i,rootIndex+1,end)
+        cout << arrInorder[i] << " ";
+
+    return nullptr;
+}
+
 int main ()
 {
 
---
+++ HW2_5.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#define For(i,start,end) for(int i {start}; i < end ; ++i)
+
+using namespace std;
+
+struct node
+{
+    int data {};
+    node* left {nullptr};
+    node* right {nullptr};
+};
+
+int findKey (int* arr, int start, int end ,int key)
+{
+    For(i,start,end)
+        if(arr[i] == key)
+            return i;
+
+    cerr << "fucckkk key not found\n";
+    return -1;
+}
+
+node* fucn (int* arrInorder,int* arrPostOrder,int start , int end)
+{
+    cout << "root :" << arrPostOrder[end-1] << '\n';
+
+    int rootIndex = findKey(arrInorder,start,end,arrPostOrder[end-1]);
+
+    cout << "left: ";
+    For(i,start,rootIndex)
+        cout << arrInorder[i] << " ";
+
+    cout << "\nright: ";
+    For(i,rootIndex+1,end)
+        cout << arrInorder[i] << " ";
+
+    return nullptr;
+}
+
+
+
+int main ()
+{
+    int n {};
+    cin >> n;
+
+    int arrInorder[n]  {};
+    int arrPostOrder[n] {};
+    For(i,0,n)
+        cin >> arrInorder[i];
+    For(i,0,n) 
+        cin >> arrPostOrder[i];
+
+    fucn(arrInorder,arrPostOrder,0,n);
+
+    return 0;
+}
